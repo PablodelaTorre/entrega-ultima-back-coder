871450cdd57ff3b7d4ff9b042817e91f82e96777
Pablo <delatorre.pablodaniel@gmail.com>
2022-10-12 12:43:03 -0300

    crud carritos funcionando a pleno

--- src/routes/routesMaster.js
+++ src/routes/routesMaster.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import productosRouter from "./routesProductos.js";
+import carritosRouter from "./routesCarritos.js";
 
 const appRouter = Router()
 
 
 appRouter.use('/productos', productosRouter)
+appRouter.use('/carritos',carritosRouter)
 appRouter.get('/',(req,res)=>{
     res.send("funcionando")
 })
