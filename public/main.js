44a72a0883834f5d99cdacb7d1774d725393ceb5
Pablo <delatorre.pablodaniel@gmail.com>
2022-10-14 21:43:33 -0300

    limpiado main.js

--- public/main.js
+++ public/main.js
@@ -1,12 +1,5 @@
 const socket = io()
 
-const inputTitle = document.getElementById("title")
-const inputPrice = document.getElementById("price")
-const inputPhoto = document.getElementById("photo")
-const button = document.getElementById("button")
-
-const tablaProducts = document.getElementById("tablaP")
-
 const inputMail = document.getElementById("mail")
 const inputName = document.getElementById("nombre")
 const inputApellido = document.getElementById("apellido")
@@ -14,20 +7,6 @@ const inputEdad = document.getElementById("edad")
 const inputMensaje = document.getElementById("mensaje")
 const mostrarMensajes = document.getElementById("nuevosMensajes")
 const buttonMensajes = document.getElementById("buttonMensajes")
-
-button.addEventListener('click', (e) => {
-    const titulo = inputTitle.value
-    const precio = inputPrice.value
-    const foto = inputPhoto.value
-    
-    const producto = {
-        titulo: titulo,
-        precio: precio,
-        foto: foto
-    }
-    console.log(producto)
-    socket.emit("newProducto", producto)
-})
 
 buttonMensajes.addEventListener('click',(e) => {
 
